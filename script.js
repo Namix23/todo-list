8c749dfcbea0e39e006f8691132d67372a438f22
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-04-01 19:10:44 -0400

    store text from the HTML input field

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const todoList = document.getElementById('todo-list');
-const addBtn = document.querySelector("#add-item");
+const addBtn = document.querySelector('#add-item');
 
 function addItem(){
-  
+  //store text from the HTML text input field
+  const textInput = document.getElementById('task-input');
+  const taskText = textInput.value;
 }
