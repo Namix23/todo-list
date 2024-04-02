b009f9a02c7d33d7cf26208b6a1cb7b250a96b74
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-04-01 23:46:56 -0400

    correct the wrong

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const todoList = document.getElementById('todo-list');
-const addBtn = document.querySelector('#add-item');
+const toDoList = document.getElementById("todo-list");
+const addBtn = document.querySelector("#add-item");
 
-function addItem(){
+function addItemToList() {
   //store text from the HTML text input field
-  const textInput = document.getElementById('task-input');
+  const textInput = document.getElementById("task-input");
   const taskText = textInput.value;
+  const newTask = document.createElement("li");
+  newTask.innerHTML = taskText;
+  //add new li element to a class
+  newTask.classList.add("todo-list-item");
+  // createCheckmarkButton(newTask);
+  // add new li
+  toDoList.append(newTask);
+  textInput.value = "";
 }
+
+addBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  addItemToList();
+});
