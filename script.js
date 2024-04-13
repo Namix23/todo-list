27b976b128bd826e80626eb1c63e15c750f0c219
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-04-12 20:16:04 -0400

    add comments to JS file

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-//
+//create fixed variables
 const toDoList = document.getElementById("todo-list");
 const addButton = document.querySelector("#add-item");
+
 
 function addItemToList() {
   //store text from the HTML text input field
   const textInput = document.getElementById("task-input");
   const taskText = textInput.value;
+
+  //create new li with the text from input field
   const newTask = document.createElement("li");
   newTask.innerHTML = taskText;
+
   //add new li element to a class
   newTask.classList.add("todo-list-item");
-
-  // createCheckmarkButton(newTask);
-
-  //add new li element as a child of the toDoList element
-  toDoList.append(newTask);
-  textInput.value = "";
-}
-// call when addButton is clicked
-addButton.addEventListener("click", function (event) {
+  
+  createCheckmarkButton();
+  addButton.addEventListener("click", function (event) {
+  });
+  const markButton = document.createElement("button");
+  markButton.innerHTML = taskText;
+  markButton.classList.add("✓");
+  
   event.preventDefault();
   addItemToList();
-});
+  markButton.addEventListener('click',function(event){
+    markButton.classList.remove("✓");
+    createCheckmarkButton();
+  })
+}
+
+//add new li element as a child of the toDoList element
+toDoList.append(newTask);
+textInput.value = "";
